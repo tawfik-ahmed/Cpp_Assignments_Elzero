47a8662a7f6f5fe995ea854cf591274fd787b769
Tawfik Ahmed <140659996+tawfik-ahmed@users.noreply.github.com>
2024-03-27 22:12:29 +0200

    Add files via upload

---
+++ week 1 assignments/week 1 assignments 3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout
+        << "Iam Learning Programming\n" << "With\n" << "Elzero\n" << "Web\n" << "School\n" << "<3 \n";
+    return 0;
+}
---
+++ week 1 assignments/week 1 assignments 4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout
+        << "|=======================|\n" << "|== Elzero Web School ==|\n" << "|==        C++        ==|\n" << "|=======================|\n";
+    return 0;
+}
